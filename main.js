10e1e2f687b448f9e0e4212987cafe3db49111cc
asadullah696 <asadullahrajpoot19@gmail.com>
2023-09-14 21:21:09 +0500

    Some practices

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+    function submit(){
+            let name = toString(document.getElementById("name"));
+            
+            let fatherName = toString(document.getElementById("father-name"));
+            
+            let sport = toString(document.getElementById("sport-name"));
+            
+
+
+             let applicantName = document.getElementById("applicant-name").textContent = name;
+             let applicantFatherName = document.getElementById("father-applicant-name").textContent = fatherName;
+             let sportName = document.getElementById("sport-name-applicant").innerText = sport;
+
+    }
