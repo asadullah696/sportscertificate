b432fe475de2656f59576dd89c9f635a3a4a040c
asadullah696 <asadullahrajpoot19@gmail.com>
2023-09-15 16:32:33 +0500

    Just to add some finishing otherwise sports certificate is complete

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+
+    
+    
+
+
+    function submit(){
+            let name = document.getElementById("name").value;
+            
+            let fatherName = document.getElementById("father-name").value;
+            
+            let sport = document.getElementById("sport-name").value;
+            
+
+
+             let applicantName = document.getElementById("applicant-name").innerText = name ;
+             let applicantFatherName = document.getElementById("father-applicant-name").textContent = fatherName;
+             let sportName = document.getElementById("sport-name-applicant").innerText = sport;
+             let applicantNameBottom = document.getElementById("applicant-name-bottom").innerText = name ;
+
+    }
