8159a84965c4cf086141239f7cc58bfbe7f45d75
asadullah696 <asadullahrajpoot19@gmail.com>
2023-09-16 21:12:29 +0500

    Little bit validation of all

--- app.js
+++ app.js
@@ -2,22 +2,25 @@
 
 
 
-    
-    
 
 
     function submit(){
-            let name = document.getElementById("name").value;
-            
-            let fatherName = document.getElementById("father-name").value;
-            
-            let sport = document.getElementById("sport-name").value;
-            
+            let myName = document.getElementById("name-input").value;
+            let fatherName = document.getElementById("father-name-input").value;
+            let sport = document.getElementById("sport-name-input").value;
+            let year = document.getElementById("year-part-input").value;
 
+            if (myName === "" || fatherName === "" || sport === "" || year === "") {
+                alert("All inputs must be filled out");
+                return false;
+            }
+        
+            // Additional validation logic can be added here for specific requirements
 
-             let applicantName = document.getElementById("applicant-name").innerText = name ;
+             let applicantName = document.getElementById("applicant-name").innerText = myName ;
              let applicantFatherName = document.getElementById("father-applicant-name").textContent = fatherName;
              let sportName = document.getElementById("sport-name-applicant").innerText = sport;
-             let applicantNameBottom = document.getElementById("applicant-name-bottom").innerText = name ;
+             let applicantNameBottom = document.getElementById("applicant-name-bottom").innerText = year ;
 
+            //  return true;
     }
