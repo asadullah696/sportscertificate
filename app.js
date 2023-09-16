73ebc1d19776bab27b114f51302e6f460d2ab7da
asadullah696 <asadullahrajpoot19@gmail.com>
2023-09-17 01:51:10 +0500

    Just Some Practice

--- app.js
+++ app.js
@@ -10,17 +10,9 @@
             let sport = document.getElementById("sport-name-input").value;
             let year = document.getElementById("year-part-input").value;
 
-            if (myName === "" || fatherName === "" || sport === "" || year === "") {
-                alert("All inputs must be filled out");
-                return false;
-            }
-        
-            // Additional validation logic can be added here for specific requirements
-
              let applicantName = document.getElementById("applicant-name").innerText = myName ;
              let applicantFatherName = document.getElementById("father-applicant-name").textContent = fatherName;
              let sportName = document.getElementById("sport-name-applicant").innerText = sport;
              let applicantNameBottom = document.getElementById("applicant-name-bottom").innerText = year ;
 
-            //  return true;
     }
